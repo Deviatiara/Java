ced33eedbfe2d612d5f033af6d9f4eb4201917e2
Teddy230998 <teddyseptian000@gmail.com>
2019-11-21 10:54:18 +0700

    Java

---
+++ Latihan 1/src/latihan/pkg1/Latihan1.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan.pkg1;
+
+/**
+ *
+ * @author PC 037
+ */
+public class Latihan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "---------------------");
+        System.out.println("NAMA\t\t: Devia Tiara Putri");
+        System.out.println("ALAMAT/t/t      : Jalan Sekarwangi");
+        System.out.println("TEMPAT LAHIR\t: Sumedang");
+        System.out.println("TANGGAL LAHIR\t: 12 November 2000");
+        System.out.println("---------------------");
+        System.out.println("HOBI\t\t: Singing");
+        System.out.println("MAKES\t\t: Bakso Sapi");
+        System.out.println("MIKES\t\t: Ice Green tea");
+    }
+    
+}
